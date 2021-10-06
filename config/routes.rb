a298dd479c858aefdc1e232e675a1121b9dbd1db
EC2 Default User <ec2-user@ip-172-31-80-200.ec2.internal>
2021-10-06 13:44:04 +0000

    4章Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'top' => 'homes#top'
- 
+Rails.application.routes.draw do 
+  get 'todolists/new'
+  get 'top' => 'homes#top'  post 'todolists' => 'todolists#cte'
 
 end
