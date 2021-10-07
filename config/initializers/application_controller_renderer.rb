66b36feb281c6cac166c260286806a98d88ef907
EC2 Default User <ec2-user@ip-172-31-80-200.ec2.internal>
2021-10-07 13:48:48 +0000

    7章Rails

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3d541a344d038ff7c5a2b59707a090b560dca6c70e2212258a791e1fff70019dc3965ae1cfddf783311c41094e15acdb953d260d719e0fcb5ec432beb9f20332'
