58f60198e84a2d459177efabf425b859ca501509
EC2 Default User <ec2-user@ip-172-31-80-200.ec2.internal>
2021-10-08 07:58:21 +0000

    9章

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do 
+Rails.application.routes.draw do
   get 'todolists/new'
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'todolists/:id' => 'todolists#show', as:'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as:'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as:'update_todolist'
+  delete 'todolists/:id' =>'todolists#destroy', as:'destroy_todolist'
   end
